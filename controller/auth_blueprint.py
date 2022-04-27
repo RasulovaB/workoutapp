f4ce12188ec00cec0bf5b5ab1e9ebbf99a4babad
RasulovaB <55116283+RasulovaB@users.noreply.github.com>
2022-04-26 21:04:57 -0400

    disable login_required function

--- controller/auth_blueprint.py
+++ controller/auth_blueprint.py
@@ -13,29 +13,29 @@ from flask_login import login_user, current_user, logout_user
 app = Blueprint("auth", __name__)
 
 
-def login_required(func):
-    """
-    Decorator function that checks if the user is authenticated.
-    It is meant to be used with routes that require authentication.
-    :param func: Function to decorate
-    :return: Either original function or decorated function
-    """
+# def login_required(func):
+#     """
+#     Decorator function that checks if the user is authenticated.
+#     It is meant to be used with routes that require authentication.
+#     :param func: Function to decorate
+#     :return: Either original function or decorated function
+#     """
 
-    @wraps(func)
-    def wrapper_func(*args, **kwargs):
-        # Check session for user log in info. If not logged in
-        if "logged_in" not in session or not session["logged_in"]:
-            # Use flash to carry over an error message to page
-            flash("Login required to access this page!")
-            # Return a redirect to login page. Set next as current page
-            # so that we can go back to it after log in
-            return func(*args, **kwargs)
-            # return redirect(url_for('auth.login', next=request.url))
+#     @wraps(func)
+#     def wrapper_func(*args, **kwargs):
+#         # Check session for user log in info. If not logged in
+#         if "logged_in" not in session or not session["logged_in"]:
+#             # Use flash to carry over an error message to page
+#             flash("Login required to access this page!")
+#             # Return a redirect to login page. Set next as current page
+#             # so that we can go back to it after log in
+#             return func(*args, **kwargs)
+#             # return redirect(url_for('auth.login', next=request.url))
 
-        # If user logged in, return original function
-        return func(*args, **kwargs)
+#         # If user logged in, return original function
+#         return func(*args, **kwargs)
 
-    return wrapper_func
+#     return wrapper_func
 
 
 @app.route("/login", methods=["POST", "GET"])
@@ -60,7 +60,7 @@ def logout():
     Function that calls completes the logout procedure.
     Redirects to main page upon log out
     """
-    logout_user()
+#     logout_user()
     return redirect(url_for("main.home"))
 
 
