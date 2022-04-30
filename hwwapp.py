30a742318618abdc477c86fb87736a19abafc5d4
RasulovaB <55116283+RasulovaB@users.noreply.github.com>
2022-04-30 14:33:50 -0400

    Update and rename hww-app.py to hwwapp.py

---
+++ hwwapp.py
@@ -0,0 +1,15 @@
+"""
+ * FILENAME: [hww-app]
+ * AUTHOR: [Team Workout App]
+ * COURSE: [CMSC 495 7383]
+ * PROFESSOR: [Jeff Sanford]
+ * CREATEDATE: [04/16/2022]
+
+"""
+
+from hiitwa import create_app
+
+
+# run file with python3 cmd
+if __name__ == "__main__":
+    create_app()
