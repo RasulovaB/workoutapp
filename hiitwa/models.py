b6ac4f3e575b8486880e9642ee8c75d37712ec35
RasulovaB <55116283+RasulovaB@users.noreply.github.com>
2022-05-01 18:52:31 -0400

    Update models.py

--- hiitwa/models.py
+++ hiitwa/models.py
@@ -18,7 +18,7 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
-    workout = db.relationship("Item", backref="author", lazy=True)
+    #workout = db.relationship("Item", backref="author", lazy=True)
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.password}')"
